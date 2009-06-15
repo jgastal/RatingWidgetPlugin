116315ebf4ac1e44248cf239ed3d51e6a708a7f5
Jonas M. Gastal <jokerwww@gmail.com>
2009-06-15 18:26:44 +0300

    Adds copyright and LGPL notice to files, plus the license itself.

--- RatingWidgetPlugin_global.h
+++ RatingWidgetPlugin_global.h
@@ -1,3 +1,21 @@
+/*
+ *  Copyright (C) 2009 Jonas Mauricio Gastal.
+ *  Contact: Jonas Mauricio Gastal (jgastal@gmail.com)
+ *
+ *  This file is part of RatingWidgetPlugin.
+ *
+ *  RatingWidgetPlugin is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License.
+ *
+ *  Parabola Biblioteca is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with RatingWidgetPlugin.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef RATINGWIDGETPLUGIN_GLOBAL_H
 #define RATINGWIDGETPLUGIN_GLOBAL_H
 
